1fdf6d58fd625334dc4aa53c4edbe3128f28d6a8
mscal <mscal4700@gmail.com>
2016-05-04 13:57:22 +1000

    Committing the only files you need for working prototype

---
+++ Assets/IntPair.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+	public class IntPair : IEquatable<IntPair>
+	{
+		public int firstDigit;
+		public int secondDigit;
+
+		public IntPair (int x, int y)
+		{
+			if (x < y) {
+
+				firstDigit = x;
+				secondDigit = y;
+
+			} else {
+
+				firstDigit = y;
+				secondDigit = x;
+
+			}
+
+		}
+
+		public bool compareDigits(IntPair other){
+			if (firstDigit == other.firstDigit && secondDigit == other.secondDigit) {
+				return true;
+			}
+			return false;
+		}
+
+		public override bool Equals (Object obj)
+		{
+			if (obj == null) {
+				return false;
+			}
+
+			IntPair pair = obj as IntPair;
+			if (pair == null) {
+				return false;
+			} else {
+				return Equals (pair);
+			}
+		}
+
+
+
+		public bool Equals(IntPair other)
+		{
+			if (other == null) {
+				return false;
+			}
+
+			return compareDigits (other);
+		}
+
+
+
+		public override int GetHashCode()
+		{
+			return 17 * this.firstDigit.GetHashCode () + 13 * this.secondDigit.GetHashCode ();
+		}
+
+		public static bool operator == (IntPair a, IntPair b)
+		{
+			if (((object)a) == null || ((object)b) == null) {
+				return Object.Equals (a, b);
+			}
+			return a.Equals (b);
+		}
+
+		public static bool operator != (IntPair a, IntPair b)
+		{
+			return !(a == b);
+		}
+
+
+
+}
+
---
+++ Assets/Scripts/SimulatedMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimulatedMovement : MonoBehaviour {
+    public float moveSpeed = 0.50f;
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, +moveSpeed, 0));
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, -moveSpeed, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-moveSpeed, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(+moveSpeed, 0, 0));
+        }
+	}
+}
