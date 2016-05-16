d4b2ef0ecc8b927844ce07a4cd7d0cae411f454a
mscal <mscal4700@gmail.com>
2016-05-17 00:25:28 +1000

    Bubble now spins faster, Also cleaned up some of the assets, Added a Memo spawner on collision(prototype, will change this later)

---
+++ Assets/SpawnMemo.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SpawnMemo : MonoBehaviour {
+    public Text funFacto;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "bubbleCollider")
+        {
+            this.gameObject.SetActive(false);
+            SetFactText();
+        }
+    }
+
+
+    void SetFactText()
+    {
+        funFacto.text = "Oreo's are more addictive compared to coke";
+    }
+}
--- Assets/rotatingBubble.cs
+++ Assets/rotatingBubble.cs
@@ -19,11 +19,12 @@ public class rotatingBubble : MonoBehaviour {
 
      private void bubbleColorChanger(){
         seconds = Mathf.FloorToInt(Time.time);
-        if (seconds >= 5 && seconds <= 5.1){
+        if (seconds >= 9 && seconds <= 9.1){
             tenSecondColor = Color.Lerp(Color.red, Color.blue, Mathf.PingPong(Time.time, 1));
             Renderer renderer = this.GetComponent<Renderer>();
             renderer.material.color = tenSecondColor;
             this.transform.localScale += new Vector3(0.005f, 0.005f, 0);
+            rotSpeed += 1f;
 
         }
     }
