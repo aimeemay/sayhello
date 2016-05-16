ee8839c33e3e94abefb0792170b456aba418c49b
mscal <mscal4700@gmail.com>
2016-05-16 20:27:30 +1000

    Added code to rotating bubble. It now transitions from blue to red when big bubble is more than 5s old. Also grows in size for .1 of a second.

--- Assets/rotatingBubble.cs
+++ Assets/rotatingBubble.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class rotatingBubble : MonoBehaviour {
 	public float rotSpeed = 10f;
+    public Color tenSecondColor = Color.red;
+    private int seconds;
+    
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +14,18 @@ public class rotatingBubble : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate (Vector3.forward, rotSpeed * Time.deltaTime);
+        bubbleColorChanger();
 	}
+
+     private void bubbleColorChanger(){
+        seconds = Mathf.FloorToInt(Time.time);
+        if (seconds >= 5 && seconds <= 5.1){
+            tenSecondColor = Color.Lerp(Color.red, Color.blue, Mathf.PingPong(Time.time, 1));
+            Renderer renderer = this.GetComponent<Renderer>();
+            renderer.material.color = tenSecondColor;
+            this.transform.localScale += new Vector3(0.005f, 0.005f, 0);
+
+        }
+    }
+
 }
