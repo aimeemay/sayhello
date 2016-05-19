ec793784b5c0dc68336d6b8a2defae82c7c5875f
mscal <mscal4700@gmail.com>
2016-05-19 12:51:29 +1000

    Please give leena code

--- Assets/rotatingBubble.cs
+++ Assets/rotatingBubble.cs
@@ -20,14 +20,18 @@ public class rotatingBubble : MonoBehaviour {
      private void bubbleColorChanger(){
         
 
-		if (Time.time - seconds >= 5 && Time.time - seconds <= 6){
-            tenSecondColor = Color.Lerp(Color.red, Color.blue, Mathf.PingPong(Time.time, 3));
+		if (Time.time - seconds >= 5 && Time.time - seconds <= 8){
+			tenSecondColor = Color.Lerp(Color.blue, Color.magenta, Mathf.PingPong(Time.time, 3));
             Renderer renderer = this.GetComponent<Renderer>();
             renderer.material.color = tenSecondColor;
             this.transform.localScale += new Vector3(0.005f, 0.005f, 0);
             rotSpeed += 1f;
 
-        }
+		}if (Time.time - seconds > 8 && Time.time - seconds <= 11) {
+
+			this.transform.localScale -= new Vector3 (0.005f, 0.005f, 0);
+
+		}
     }
 
 }
