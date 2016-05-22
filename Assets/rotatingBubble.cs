81679d1ce9be1752c9745d969d262c0d88b32525
mscal <mscal4700@gmail.com>
2016-05-22 23:35:26 +1000

    Big bub now scales properly.Although ill probably stop it from going to 0 scale and have it oscillate between a min value. Ugly colour changed(its still ugly). Gave new prefabs higher order in layer so i can see them. git push

--- Assets/rotatingBubble.cs
+++ Assets/rotatingBubble.cs
@@ -5,7 +5,8 @@ public class rotatingBubble : MonoBehaviour {
 	public float rotSpeed = 10f;
     public Color tenSecondColor = Color.red;
     private int seconds;
-    
+    private float rate = 0.15f;
+    private float ratio = 0.15f;
 	// Use this for initialization
 	void Start () {
 		seconds = Mathf.FloorToInt(Time.time);
@@ -20,16 +21,17 @@ public class rotatingBubble : MonoBehaviour {
      private void bubbleColorChanger(){
         
 
-		if (Time.time - seconds >= 5 && Time.time - seconds <= 8){
-			tenSecondColor = Color.Lerp(Color.blue, Color.magenta, Mathf.PingPong(Time.time, 3));
+		if (Time.time - seconds >= 5){
+			tenSecondColor = Color.Lerp(Color.blue, Color.cyan, Mathf.PingPong(Time.time, 3));
             Renderer renderer = this.GetComponent<Renderer>();
             renderer.material.color = tenSecondColor;
-            this.transform.localScale += new Vector3(0.005f, 0.005f, 0);
-            rotSpeed += 1f;
-
-		}if (Time.time - seconds > 8 && Time.time - seconds <= 11) {
-
-			this.transform.localScale -= new Vector3 (0.005f, 0.005f, 0);
+            //this.transform.localScale += new Vector3(0.001f, 0.001f, 0);
+            this.transform.localScale = Mathf.PingPong(Time.time * rate, ratio) * Vector3.one;
+            rotSpeed += 0.5f;
+        }if (Time.time - seconds > 10) {
+            rotSpeed = 40f;
+            //this.transform.localScale = new Vector3(0.15f, 0.15f, 0);
+			//this.transform.localScale -= new Vector3 (0.005f, 0.005f, 0);
 
 		}
     }
